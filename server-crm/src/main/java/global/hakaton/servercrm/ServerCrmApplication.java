ba8dc9591f4204baa04cab6da045d33f213a3eb8
Mono <misupriority@gmail.com>
2024-09-23 20:22:39 +0500

    Initial Commit

---
+++ server-crm/src/main/java/global/hakaton/servercrm/ServerCrmApplication.java
@@ -0,0 +1,13 @@
+package global.hakaton.servercrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerCrmApplication.class, args);
+    }
+
+}
---
+++ server-crm/src/test/java/global/hakaton/servercrm/ServerCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package global.hakaton.servercrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
