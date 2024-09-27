cb806c246067bd0e36c2fc2178d3cb1896a1f4bb
Timluk1 <118183087+Timluk1@users.noreply.github.com>
2024-09-27 21:10:12 +0300

    Add pages and forms registration and login

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,18 @@
-function LoginPage() {
-  return (
-    <div>LoginPage</div>
-  )
-}
+import React from "react";
+import LoginForm from "../components/Auth/LoginForm";
+import { Content } from "antd/es/layout/layout";
+import Layout from "antd/es/layout/layout";
+
+const LoginPage: React.FC = () => {
+    return (
+        <Layout className="mt-20">
+            <Content className="p-[50px]">
+                <div className="w-[100%]">
+                    <LoginForm />
+                </div>
+            </Content>
+        </Layout>
+    );
+};
 
 export default LoginPage;
--- frontend/src/pages/ReigstrationPage.tsx
+++ frontend/src/pages/ReigstrationPage.tsx
@@ -1,7 +1,19 @@
-function ReigstrationPage() {
-  return (
-    <div>ReigstrationPage</div>
-  )
-}
+import React from "react";
+import { Layout } from "antd";
+import RegistrationForm from "../components/Auth/RegistrationForm";
 
-export default ReigstrationPage;
+const { Content } = Layout;
+
+const RegistrationPage: React.FC = () => {
+    return (
+        <Layout className="mt-20">
+            <Content className="p-[30px]">
+                <div className="w-full">
+                    <RegistrationForm />
+                </div>
+            </Content>
+        </Layout>
+    );
+};
+
+export default RegistrationPage;
