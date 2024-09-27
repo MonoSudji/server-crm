eab7da72a51e28fbad143604a78c57c517f15c67
Timluk1 <118183087+Timluk1@users.noreply.github.com>
2024-09-28 02:46:09 +0300

    Change profile

--- frontend/src/store/settings/settingsSlice.ts
+++ frontend/src/store/settings/settingsSlice.ts
@@ -2,18 +2,15 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const THEME_IS_DARK = localStorage.getItem("THEME_IS_DARK");
 const LANGUAGE = localStorage.getItem("LANGUAGE")
-const PANEL_TRANSPARENCY = localStorage.getItem("PANEL_TRANSPARENCY")
 
 export interface SettingsState {
     themeIsDark: boolean;
     language: string;
-    panelTransparency: string;
 }
 
 const initialState: SettingsState = {
     themeIsDark: THEME_IS_DARK ? Boolean(THEME_IS_DARK) : false,
     language: LANGUAGE ? LANGUAGE : "Русский",
-    panelTransparency: PANEL_TRANSPARENCY ? PANEL_TRANSPARENCY : "transparent",
 }
 
 export const settingsSlice = createSlice({
@@ -24,10 +21,6 @@ export const settingsSlice = createSlice({
             state.themeIsDark = !state.themeIsDark;
             localStorage.setItem("THEME_IS_DARK", String(state.themeIsDark));
         },
-        setPanelTransparency(state, action) {
-            state.panelTransparency = action.payload;
-            localStorage.setItem("PANEL_TRANSPARENCY", state.panelTransparency);
-        },
         setLanguage(state, action) {
             state.language = action.payload;
             localStorage.setItem("LANGUAGE", state.language);
@@ -36,6 +29,6 @@ export const settingsSlice = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { setTheme, setPanelTransparency, setLanguage } = settingsSlice.actions;
+export const { setTheme, setLanguage } = settingsSlice.actions;
 
 export default settingsSlice.reducer
